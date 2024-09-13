b4e56f968f5226471ff72963ceb51c2310fd0baf
juanmafari <108095147+JuanOttonelli@users.noreply.github.com>
2024-09-13 12:59:14 -0300

    cambio clase enano y diagrama de clases actualizado

--- src/Library/Enano.cs
+++ src/Library/Enano.cs
@@ -6,9 +6,22 @@ using System.Collections.Generic;
 public class Enano
 {
     // Propiedades del enano
-    public string Nombre { get; set; } // Nombre del enano
-    public int Vida { get; set; } // Vida del enano
-    public List<Item> Items { get; set; }  // Lista de items que el enano posee
+    private string Nombre { get; set; } // Nombre del enano
+    private int Vida;  // Vida del enano
+
+    public int vida
+    {
+        get { return Vida; } 
+        set { Vida = value; }
+    }
+
+    private int VidaInicial;
+
+    public int vidaInicial
+    {
+        get { return VidaInicial; }
+    }
+    public List<Item> ItemsList { get; set; }  // Lista de items que el enano posee
     public int Fuerza { get; set; } // Fuerza base del enano
 
     // Constructor de la clase Enano
@@ -16,30 +29,69 @@ public class Enano
     {
         Nombre = nombre; // Asigna el nombre
         Vida = vida; // Asigna la vida
+        VidaInicial = vida;
         Fuerza = fuerza; // Asigna la fuerza
-        Items = new List<Item>(); // Inicializa la lista de items vacía
+        ItemsList = new List<Item>(); // Inicializa la lista de items vacía
     }
 
     // Método para agregar un item al enano
     public void AgregarItem(Item item)
     {
-        Items.Add(item); // Añade el item a la lista de items
+        ItemsList.Add(item); // Añade el item a la lista de items
     }
 
     // Método para quitar un item del enano
     public void QuitarItem(Item item)
     {
-        if (Items.Contains(item)) // Verifica si el item está en la lista
+        if (ItemsList.Contains(item)) // Verifica si el item está en la lista
         {
-            Items.Remove(item); // Si está, lo elimina
+            ItemsList.Remove(item); // Si está, lo elimina
         }
     }
 
+    public void Atacar(Object personaje, Item item)
+    {
+        if (personaje is Enano)
+        {
+            Enano enano = (Enano)personaje;    
+            enano.vida -= item.ataque;
+        }
+        else if (personaje is Elfo)
+        {
+            Elfo elfo = (Elfo)personaje;    
+            elfo.vida -= item.ataque;
+        }
+        else if (personaje is Mago)
+        {
+            Mago mago = (Mago)personaje;    
+            mago.vida -= item.ataque;
+        }
+        
+    }
+    public void Curar(Object personaje)
+    {
+        if (personaje is Enano)
+        {
+            Enano enano = (Enano)personaje;    
+            enano.vida = enano.vidaInicial;
+        }
+        else if (personaje is Elfo)
+        {
+            Elfo elfo = (Elfo)personaje;    
+            elfo.vida = elfo.vidaInicial;
+        }
+        else if (personaje is Mago)
+        {
+            Mago mago = (Mago)personaje;    
+            mago.vida = mago.vidaInicial;
+        }
+        
+    }
     // Método que calcula el ataque total sumando los ataques de todos los items y la fuerza del enano
-    public int Atacar()
+    public int getAtaqueTotal()
     {
         int ataqueTotal = 0;
-        foreach (var item in Items) // Itera sobre los items del enano
+        foreach (var item in ItemsList) // Itera sobre los items del enano
         {
             ataqueTotal += item.ataque; // Suma el valor de ataque de cada item
         }
@@ -48,10 +100,10 @@ public class Enano
     }
 
     // Método que calcula la defensa total sumando las defensas de todos los items
-    public int Defender()
+    public int getDefensaTotal()
     {
         int defensaTotal = 0;
-        foreach (var item in Items) // Itera sobre los items del enano
+        foreach (var item in ItemsList) // Itera sobre los items del enano
         {
             defensaTotal += item.defensa; // Suma el valor de defensa de cada item
         }
