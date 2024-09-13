7c9efc65a439264cdd45f6cb068f1a4f5f04769f
juanmafari <108095147+JuanOttonelli@users.noreply.github.com>
2024-09-13 20:07:16 -0300

    Program con ejemplos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace ucu;
+
+/// <summary>
+/// El programa principal.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// El punto de entrada al programa.
+    /// </summary>
+    public static void Main()
+    {
+        
+        Enano dwarv = new Enano("Dwarven Stepworld", 60);
+        Enano fasto = new Enano("Faston Sellsword", 85);
+        
+        Item pipa = new Item("Pipa del enano", 0, 10, "Life boost");
+        Item faca = new Item("Faca afilada", 12, 0, "Short distance");
+        
+        fasto.AgregarItem(faca);
+        fasto.QuitarItem(faca);
+        dwarv.AgregarItem(faca);
+        dwarv.Atacar(fasto,faca);
+        dwarv.Curar(fasto);
+        Console.WriteLine(fasto.vida);
+        
+    }
+    
+
+    
+}
